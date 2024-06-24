26b35b694f3946b79ee3cf5ccfa8390322531027
Vitoria <108755321+vitoriapalmeirao@users.noreply.github.com>
2024-06-24 11:42:00 -0300

    IA7

--- script.js
+++ script.js
@@ -78,11 +78,7 @@ const perguntas = [
             }
         "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
             "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
-        ]
-    }
-];
-
-let atual = 0;
+        let atual = 0;
 let perguntaAtual;
 
 function mostraPergunta() {
